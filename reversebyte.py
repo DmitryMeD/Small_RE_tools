ea1950ddb12b61dc117cb1f3a98d078c8afafb56
DmitryMeD <75813507+DmitryMeD@users.noreply.github.com>
2022-01-02 15:32:37 +0100

    Add files via upload

---
+++ reversebyte.py
@@ -0,0 +1,34 @@
+import sys
+
+def reversebyte(dump,dumpout):
+  c = len(dump)
+  c -= 1
+  for i in range (len(dump)):
+    dumpout[i] += dump[c]
+    i += 1
+    c -= 1
+  return dumpout
+
+def openfile (s):
+  sys.stderr.write(s + "\n")
+  sys.stderr.write("Usage: %s <infile> <outfile>\n" % sys.argv[0])
+  sys.exit(1)
+
+if __name__ == '__main__':
+  
+  if len(sys.argv) != 3:
+    openfile("invalid argument count")
+  outfile = sys.argv.pop()
+  infile  = sys.argv.pop()
+  
+  file = open(infile,"rb")
+  
+  dump = bytearray(file.read())
+  dumpout = bytearray(len(dump))
+  outdump = reversebyte(dump,dumpout)
+  print(dumpout)
+  
+  new = open(outfile,"wb")
+  new.write(outdump)
+  new.close()
+  file.close()
